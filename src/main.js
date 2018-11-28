22a0b0fa8e907174d96702cf253a3c3caaed8309
水红花子 <songmh@igancao.com>
2018-11-28 17:01:54 +0800

    引入babel-polyfill

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import "babel-polyfill";//<--为了将es6编译后更兼容ios4.4以下的版本
 import Vue from 'vue'
 import App from './App'
 import router from './router'

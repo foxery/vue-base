3f007ea2bcdc4b8e6d91264b5933faa067f25166
水红花子 <songmh@igancao.com>
2018-12-04 16:39:52 +0800

    添加axios响应拦截器

--- src/main.js
+++ src/main.js
@@ -2,11 +2,30 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import "babel-polyfill";//<--为了将es6编译后更兼容ios4.4以下的版本
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store/index';
 
 Vue.config.productionTip = false
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
 
 /* eslint-disable no-new */
 new Vue({
